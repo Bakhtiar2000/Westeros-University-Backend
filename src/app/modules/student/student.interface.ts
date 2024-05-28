f4f377f82ad2746815912b4925360ace660e8e8b
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-05-28 18:30:20 +0600

    Added student model, interface, services, routes and ontroller

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,39 @@
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNumber: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNumber: string;
+};
+
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+//------------------Student interface----------------------
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  email: string;
+  dateOfBirth: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup: 'A+' | 'A-' | 'B+' | 'A+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: 'active' | 'blocked';
+};
