d01d4f9ad459047f6b0897590be62d0de2936ae3
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-05-28 16:56:14 +0600

    added ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ async function main() {
     await mongoose.connect(config.database_url as string); // As we are sure that this database_url is string, we forcefully make it's type string. Otherwise there could be a type error
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`App listening on port ${config.port}`);
     });
   } catch (err) {
     console.log(err);
