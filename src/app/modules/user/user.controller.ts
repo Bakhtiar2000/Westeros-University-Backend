ed93d386447e6974d5e390e606e90d91f3407e47
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-06-03 00:47:19 +0600

    added catchAsync

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,49 +1,20 @@
-// import studentValidationSchema from './student.validation.zod';
-import { NextFunction, Request, Response } from 'express';
 import { UserServices } from './user.service';
 import sendResponse from '../../utils/sendResponse';
 import httpStatus from 'http-status';
+import { catchAsync } from '../../utils/catchAsync';
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { password, student: studentData } = req.body;
+const createStudent = catchAsync(async (req, res) => {
+  const { password, student: studentData } = req.body;
+  const result = await UserServices.createStudentIntoDB(password, studentData);
 
-    // Validating user data using joi
-    // const { error, value } = studentValidationSchema.validate(studentData);
-
-    // Validating user data using zod
-    // const validatedData = studentValidationSchema.parse(studentData);
-
-    // if (error) {
-    //   res.status(500).json({
-    //     success: false,
-    //     message: 'Something went wrong',
-    //     error: error.details,
-    //   });
-    // }
-
-    // Putting the validated data in result
-    const result = await UserServices.createStudentIntoDB(
-      password,
-      studentData,
-    );
-
-    //Send Response
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student is created successfully',
-      data: result,
-    });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (err) {
-    next(err);
-  }
-};
+  //Send Response
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is created successfully',
+    data: result,
+  });
+});
 
 export const UserControllers = {
   createStudent,
