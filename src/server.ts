36b54029194198cb7c7ece1e18b565011c7d1cbd
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-06-05 20:23:33 +0600

    handled unhandled Rejection and uncaught exception

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 import config from './app/config';
 import app from './app';
+import { Server } from 'http';
+
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string); // As we are sure that this database_url is string, we forcefully make it's type string. Otherwise there could be a type error
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`App listening on port ${config.port}`);
     });
   } catch (err) {
@@ -15,3 +18,22 @@ async function main() {
 }
 
 main();
+
+// Handling unhandled Rejection (Asynchronous code)
+process.on('unhandledRejection', () => {
+  console.log('👍👍 unhandled Rejection detected. Shutting down 👍👍');
+  if (server) {
+    server.close(() => {
+      process.exit(1); // If server runs any asynchronous code, we need to let it finish the process and then shut off
+    });
+  }
+  process.exit(1); // If server has no running process, then it can immediately shut off
+});
+
+// Handling Uncaught Exception (Synchronous code)
+process.on('uncaughtException', () => {
+  console.log('👍👍 Uncaught Exception detected. Shutting down 👍👍');
+  process.exit(1); // Shut off immediately when it is a synchronous code
+});
+
+// console.log(x);
