63aa91554ea7171bb2291656fe19a60a17555d9a
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-06-10 11:46:58 +0600

    validation added for faculty time/ day conflict in offered course

--- src/app/modules/offeredCourse/offeredCourse.service.ts
+++ src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -8,6 +8,7 @@ import { AcademicDepartment } from '../academicDepartment/academicDepartment.mod
 import { Course } from '../course/course.model';
 import { Faculty } from '../faculty/faculty.model';
 import { AcademicSemester } from '../academicSemester/academicSemester.model';
+import QueryBuilder from '../../builder/QueryBuilder';
 
 const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
   const {
@@ -17,6 +18,9 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
     course,
     faculty,
     section,
+    days,
+    startTime,
+    endTime,
   } = payload;
 
   const isSemesterRegistrationExists =
@@ -28,7 +32,7 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
     );
   }
 
-  // Academic semester is  will not come from front end data (payload), It will be retrieved from semester registration
+  // Academic semester will not come from front-end data (payload), It will be retrieved from semester registration
   const academicSemester = isSemesterRegistrationExists.academicSemester;
 
   const isAcademicFacultyExists =
@@ -53,7 +57,7 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
     throw new AppError(httpStatus.NOT_FOUND, 'Faculty not found !');
   }
 
-  //Check if the department belongs to the faculty
+  //----------------------Check if the department belongs to the faculty-----------------------------
   const isDepartmentBelongToFaculty = await AcademicDepartment.findOne({
     _id: academicDepartment,
     academicFaculty,
@@ -65,7 +69,7 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
     );
   }
 
-  //Check if the requested course with same section already offered in given semester
+  //---------------------Check if the requested course with same section already offered in given semester--------------------------
   const isOfferedCourseExistsWithSameSectionInThisSemesterRegistration =
     await OfferedCourse.findOne({
       semesterRegistration,
@@ -81,16 +85,62 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
     );
   }
 
+  //--------------------Get the schedules of the faculties that have classes in the same days in payload data---------------------------
+  const existingSchedules = await OfferedCourse.find({
+    semesterRegistration,
+    faculty,
+    days: { $in: days }, // filtering existing schedules of a faculty in a semester for only the days requested in the payload
+  }).select('days startTime endTime');
+
+  const requestedSchedule = { days, startTime, endTime }; // From payload
+
+  existingSchedules.forEach((schedule) => {
+    const existingStartTime = new Date(`1970-01-01T${schedule.startTime}`);
+    const existingEndTime = new Date(`1970-01-01T${schedule.endTime}`);
+    const requestedStartTime = new Date(
+      `1970-01-01T${requestedSchedule.startTime}`,
+    );
+    const requestedEndTime = new Date(
+      `1970-01-01T${requestedSchedule.endTime}`,
+    );
+
+    //Let's Consider:
+    // existingTime: 11.30 - 12.30 ---- requestedTime: 11.00 - 12.00 [Conflict]
+    // existingTime: 09.30 - 10.30 ---- requestedTime: 11.00 - 12.00 [No Conflict]
+
+    if (
+      requestedStartTime < existingEndTime &&
+      requestedEndTime > existingStartTime
+    ) {
+      throw new AppError(
+        httpStatus.CONFLICT,
+        `Faculty ${isFacultyExists.name.firstName} ${isFacultyExists.name.middleName} ${isFacultyExists.name.lastName} is not available at the requested time/ day!`,
+      );
+    }
+  });
+
   const result = await OfferedCourse.create({ ...payload, academicSemester });
   return result;
 };
 
 const getAllOfferedCoursesFromDB = async (query: Record<string, unknown>) => {
-  console.log(query);
+  const offeredCourseQuery = new QueryBuilder(
+    OfferedCourse.find()
+      .populate('academicFaculty')
+      .populate('academicDepartment'),
+    query,
+  )
+    .filter()
+    .paginate()
+    .sort()
+    .fields();
+  const result = await offeredCourseQuery.modelQuery;
+  return result;
 };
 
 const getSingleOfferedCourseFromDB = async (id: string) => {
-  console.log(id);
+  const result = await OfferedCourse.findById(id);
+  return result;
 };
 
 const updateOfferedCourseIntoDB = async (
