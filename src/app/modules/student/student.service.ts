cf898f7ab36c11c2e272e6297c32bf9beaf8e228
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2025-01-10 20:28:18 +0600

    updated

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -92,6 +92,7 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
 const getSingleStudentFromDB = async (id: string) => {
   const result = await Student.findById(id)
     .populate('admissionSemester')
+    .populate('user')
     .populate({
       path: 'academicDepartment',
       populate: {
